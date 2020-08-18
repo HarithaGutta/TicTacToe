4d67e8688eb0c15bf88a48e01e8bbfaff8fd5598
HarithaGutta <haritha.sk17@gmail.com>
2020-08-18 22:37:52 +0530

    stepNumber& key added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,41 +2,17 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-        squares:Array(9).fill(null),
-        xIsNext:true,
-        }
-    }
+    
 renderSquare(i)
 {
-    return <Square 
-    value={this.state.squares[i]} 
-    onClick={()=>this.handleClick(i)}/>;
+    return (<Square 
+    value={this.props.squares[i]} 
+    onClick={()=>this.props.onClick(i)}/>);
 }
-handleClick(i)
-{
-    const squares=this.state.squares.slice()
-    if(calculateWinner(squares)){
-        return;
-    }
-    squares[i]=this.state.xIsNext ?'X':'O';
-    this.setState({squares:squares,
-    xIsNext:!this.state.xIsNext})
-}
+
 render(){
-    const winner= calculateWinner(this.state.squares)
-    let status;
-    if(winner){
-        status="Winner is : "+ winner;
-    }
-    else{
-        status='Next Player : '+(this.state.xIsNext?'X':'O');
-    }
     return(
         <div>
-            <div className="player">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -53,27 +29,7 @@ render(){
                     {this.renderSquare(8)}
                 </div>
         </div>
-    )
+    );
 }
 }
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
 export default Board;
